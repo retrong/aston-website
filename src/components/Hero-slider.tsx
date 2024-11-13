032ecc657cfd8ff6c30ddaba9d238b389475d5a0
retrong <eguobabarth@gmail.com>
2024-11-13 11:36:40 +0100

    modified hero component

--- src/components/Hero-slider.tsx
+++ src/components/Hero-slider.tsx
@@ -82,7 +82,7 @@ const HeroSlider = () => {
               loading="lazy"
             />
 
-            <div className="absolute inset-0 bg-black opacity-75 backdrop-blur-xl"></div>
+            <div className="absolute inset-0 bg-black opacity-50 backdrop-blur-xl"></div>
             {/* <div className="absolute inset-0 flex items-center justify-center">
               <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold text-white bg-black bg-opacity-50 px-4 py-2 rounded">
                 {slide.caption}
